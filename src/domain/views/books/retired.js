2c0883fb60f225a39b11071591db976228a36e61
Sasha Jolich <sasha@ctoatlarge.com>
2022-06-08 14:36:52 -0700

    feat: use Persistr's document storage for views

--- src/domain/views/books/retired.js
+++ src/domain/views/books/retired.js
@@ -1,28 +1,34 @@
 module.exports = {
   events: [ 'gifted', 'retired' ],
   on: async (toolbox, view, event) => {
-    const { collection } = toolbox
+    const { docs } = toolbox
 
     // Get the book, or create one if it doesn't already exist.
-    let book = await collection.findOne({ id: event.meta.stream })
+    let book = await docs('books').read({ id: event.meta.stream })
     if (!book) book = { ...event.data, id: event.meta.stream }
 
     // Mark the book as retired.
     book.retired = (event.meta.type === 'retired')
 
-    // Update (or insert) the book in the collection.
-    await collection.update({ id: event.meta.stream }, book, { upsert: true })
+    // Write the book to the collection.
+    await docs('books').write(book)
   },
   get: async (toolbox, view, options) => {
-    const { collection } = toolbox
+    const { docs } = toolbox
 
     // Set paging defaults.
     if (!options.page_size) options.page_size = 2
     if (!options.page_num) options.page_num = 1
 
     // Return matching books at the given page.
-    const skips = options.page_size * (options.page_num - 1)
-    const books = await collection.find({ retired: true }, { skip: Number(skips), limit: Number(options.page_size) }).toArray()
+    const skip = options.page_size * (options.page_num - 1)
+    const books = await docs('books').find({ retired: true }, { skip, limit: options.page_size })
+
+    // TODO: Sorting is not working just yet.
+    //const books = await docs('books').find({ retired: true }, { skip, limit: options.page_size, sort: 'name' })
+    //const books = await docs('books').find({ retired: true }, { skip, limit: options.page_size, sort: { by: 'name', order: 'asc' }})
+    //const books = await docs('books').find({ retired: true }, { skip, limit: options.page_size, sort: [ 'author', { by: 'name', order: 'asc' } ]})
+
     return { books }
   }
 }
